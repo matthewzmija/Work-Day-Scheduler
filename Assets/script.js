21778c594eba76143eeed0a2bb5af16ba31117da
Matthew Zmija <matthewzmija@Monicas-MacBook-Air.local>
2023-04-06 19:26:35 -0400

    add comments

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,4 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
+// ID attribute is used as a key to save the user input in local storage
 dayjs.extend(window.dayjs_plugin_advancedFormat);
 $(function () {
   $(".btn").click(function () {
@@ -14,12 +6,8 @@ $(function () {
     var textVal = $(this).parent().children("textarea").val();
     localStorage.setItem(idTimeBlock, textVal);
   });
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+
+  // Loop compares then applies the past, present, or future class to each time block
   for (var i = 9; i < 18; i++) {
     if (i < dayjs().format("H")) {
       $("#hour-" + i).addClass("past");
@@ -29,16 +17,14 @@ $(function () {
       $("#hour-" + i).addClass("present");
     }
   }
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
+
+  // User input that was saved in localStorage and sets the values of the corresponding textarea elements
   for (var i = 9; i < 18; i++) {
     $("#hour-" + i)
       .children("textarea")
       .val(localStorage.getItem("hour-" + i));
   }
-  //
-  // TODO: Add code to display the current date in the header of the page.
+
+  // Display current date in the header
   $("#currentDay").text(dayjs().format("dddd, MMMM, Do"));
 });
